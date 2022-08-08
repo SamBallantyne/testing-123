31c915169bd9559218bb89d8acf893ddd670722d
Sam Ballantyne <sam.ballantyne@gmail.com>
2022-08-07 21:19:22 -0400

    Update EventIdProvider.swift

--- Sources/EventIdProvider.swift
+++ Sources/EventIdProvider.swift
@@ -1,1 +1,43 @@
+import SwiftUI
 
+struct HeartbeatMsg : Codable {
+    var latestEventId: Int
+}
+
+class EventIdProvider : ObservableObject {
+    
+    let _publishableKey: String
+    
+    let urlSession = URLSession(configuration: .default)
+    
+    @Published var eventId : Int? = nil
+    @Published var isConnected : Bool = false
+    
+    private func connect () {
+        let url = URL(string: "wss://backend.shanghai.technology?authKey=\(_publishableKey)")!
+        let webSocketTask = urlSession.webSocketTask(with: url)
+        webSocketTask.receive { result in
+            switch result {
+            case .failure(let error):
+                print("Failed to receive message: \(error)")
+            case .success(let message):
+                switch message {
+                case .string(let text):
+                    if let msg = try? JSONDecoder().decode(HeartbeatMsg.self, from: Data(text.utf8)) {
+                        self.eventId = msg.latestEventId
+                    }
+                case .data(let data):
+                    print("Received unexpected data: \(data)")
+                @unknown default:
+                    fatalError()
+                }                
+            }
+        }
+        webSocketTask.resume()
+    }
+    
+    init(publishableKey: String) {
+        _publishableKey = publishableKey
+        connect();
+    }
+}
